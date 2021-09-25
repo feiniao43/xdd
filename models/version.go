fec74c9fb3a48bd2b95f02e6d10ecb32f0622ba8
feiniao43 <85214867+feiniao43@users.noreply.github.com>
2021-09-25 19:33:26 +0800

    Update version.go

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "2021092409"
+var version = "2021092509"
 var describe = "测试更新"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
